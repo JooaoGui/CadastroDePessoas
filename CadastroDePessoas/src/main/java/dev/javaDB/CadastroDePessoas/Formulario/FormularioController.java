10708235b19cef5c6d035a56c5418c7959f16b3b
João <guigamers666@gmail.com>
2025-03-28 23:04:26 -0300

    fix: Altera o nome das rotas controller do formulario

--- CadastroDePessoas/src/main/java/dev/javaDB/CadastroDePessoas/Formulario/FormularioController.java
+++ CadastroDePessoas/src/main/java/dev/javaDB/CadastroDePessoas/Formulario/FormularioController.java
@@ -3,25 +3,25 @@ package dev.javaDB.CadastroDePessoas.Formulario;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("formulario")
+@RequestMapping("/formulario")
 public class FormularioController {
 
     // Post -- Mandar uma requisicao para criar
-    @PostMapping("criar")
+    @PostMapping("/adicionar")
     public String criarFormulario(){
         return "Formulario criado com Sucesso";
-    }
-
-    // Put -- Mandar uma requisicao para alterar
-    @PutMapping("/alterar")
-    public String alterarFormulario(){
-        return "Formulario alterado com sucesso";
     }
 
     // GET - Mandar uma requisicao para mostrar
     @GetMapping("/listar")
     public String listarFormulario(){
         return "Formulario listados com sucesso";
+    }
+
+    // Put -- Mandar uma requisicao para alterar
+    @PutMapping("/alterar")
+    public String alterarFormulario(){
+        return "Formulario alterado com sucesso";
     }
 
     // DELETE -- Mandar uma requisicao para deletar
